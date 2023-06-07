37e0f2baf7e70d1d51aed615f571984d0ce02ad5
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-06-07 12:02:52 +0700

    Add test Eleventh

--- main.py
+++ main.py
@@ -33,46 +33,46 @@ class Predict(Resource):
     def post(self):
         try:
             # Load Dataset
-            # pd.read_excel('Words_Newest_2.0.xlsx')
+            ds = pd.read_excel('Words_Newest_2.0.xlsx', engine='openpyxl')
 
-            # # Remove Unnamed Column
-            # ds = ds.loc[:,~ds.columns.str.match("Unnamed")]
+            # Remove Unnamed Column
+            ds = ds.loc[:,~ds.columns.str.match("Unnamed")]
 
-            # # Fixes
-            # ds.dropna(subset=['Skor'], inplace=True)
+            # Fixes
+            ds.dropna(subset=['Skor'], inplace=True)
 
-            # # Change Skor to Integer
-            # ds['Skor'] = ds['Skor'].astype(int)
-            # X_train, X_test, y_train, y_test = train_test_split(ds.Kalimat, ds.Skor, random_state=1)    
+            # Change Skor to Integer
+            ds['Skor'] = ds['Skor'].astype(int)
+            X_train, X_test, y_train, y_test = train_test_split(ds.Kalimat, ds.Skor, random_state=1)    
 
             # # Call Vectorizer
             v =CountVectorizer()
 
-            # # Fit TRansform
-            # X_train_count = v.fit_transform(X_train)
-            # X_test_count = v.transform(X_test)
+            # Fit TRansform
+            X_train_count = v.fit_transform(X_train)
+            X_test_count = v.transform(X_test)
 
-            # # Import Logreg
-            # from sklearn.linear_model import LogisticRegression
+            # Import Logreg
+            from sklearn.linear_model import LogisticRegression
 
-            # # Variabel Logreg
-            # logreg = LogisticRegression()
+            # Variabel Logreg
+            logreg = LogisticRegression()
 
-            # # Fit
-            # logreg.fit(X_train_count, y_train)
+            # Fit
+            logreg.fit(X_train_count, y_train)
 
             # Get Request Data
             data = request.get_json()
 
             if request.method == 'POST':
-                    # # Transform Sent Word
-                    # vect = v.transform([data['kalimat']])
+                    # Transform Sent Word
+                    vect = v.transform([data['kalimat']])
 
-                    # # Predict
-                    # my_prediction = logreg.predict(vect)
-
+                    # Predict
+                    my_prediction = logreg.predict(vect)
+                    # print(my_prediction[0])
                     # Result
-                    return jsonify({'Result': data['kalimat']})
+                    return jsonify({'Result': my_prediction.tolist()[0]})
 
         except Exception as error:
             return {'error': error}
