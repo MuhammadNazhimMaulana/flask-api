3907de64eeade2b3b7e78702ef9bf9fd49173731
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-08-13 14:15:22 +0700

    Test Update

--- main.py
+++ main.py
@@ -19,5 +19,6 @@ app.register_blueprint(home_bp)
 app.register_blueprint(prediction_bp)
 
 if __name__ == "__main__":
+    from waitress import serve
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    serve(app, host='0.0.0.0', port=port)
